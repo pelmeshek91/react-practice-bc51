577a66e7cc422b837e9f7d627da0898ab4be0af5
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-05-19 17:04:08 +0100

    some examples from notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+
+const favouriteBooks = [
+  { id: 'id-1', name: 'JS for beginners' },
+  { id: 'id-2', name: 'React basics' },
+  { id: 'id-3', name: 'React Router overview' },
+  { id: 'id-4', name: 'Redux in depth' },
+];
+
+const BookList = ({ books }) => {
+  return (
+    <ul>
+      {books.map(book => (
+        <li>{book.name}</li>
+      ))}
+    </ul>
+  );
+};
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <BookList books={favouriteBooks} />
     </div>
   );
 };
