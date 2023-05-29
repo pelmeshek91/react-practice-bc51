832ca712a3229bbed51f9f5f72b62c02480f9223
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-05-29 14:41:42 +0100

    add lifecycle

--- src/components/usersList/UsersList.jsx
+++ src/components/usersList/UsersList.jsx
@@ -1,17 +1,12 @@
-// const UsersList = ({users}) => {
-//     const elements = users.map(({id ,name , username , email , avatarUrl , address:{street, city}}) => {
-//         return <li key={id} >
-//             <p>Name: {name}</p>
-//             <p>UserName: {username}</p>
-//             <p>email: {email}</p>
-//             <img src={avatarUrl}/>
-//             <p>address:{street},{city} </p>
-//         </li>
-//     })
 import PropTypes from 'prop-types';
 import { UserItem } from '../User/UserItem';
 
-export const UsersList = ({ users, userDelete, changeStat }) => {
+export const UsersList = ({
+  users,
+  userDelete,
+  changeStat,
+  showUserDetails,
+}) => {
   return (
     <ul>
       {users.map(user => (
@@ -19,6 +14,7 @@ export const UsersList = ({ users, userDelete, changeStat }) => {
           key={user.id}
           userDelete={userDelete}
           changeJob={changeStat}
+          showUserDetails={showUserDetails}
           {...user}
         />
       ))}
@@ -34,4 +30,7 @@ UsersList.propTypes = {
       ]),
     }).isRequired
   ).isRequired,
+  userDelete: PropTypes.func.isRequired,
+  changeStat: PropTypes.func.isRequired,
+  showUserDetails: PropTypes.func.isRequired,
 };
